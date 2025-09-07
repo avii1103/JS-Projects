632c2d8e8375d64a9ca6591b3497fcde53b8614f
avii1103 <avichal1108@gmail.com>
2025-09-07 20:00:58 +0530

    stopwatch-main

--- Stopwatch/index.js
+++ Stopwatch/index.js
@@ -1,5 +1,33 @@
-function startclock() {}
+let secondselapsed=0;
+let interval=null;
+const time=document.getElementById("time");
 
-function stopclock() {}
+function padding(value); {
+    return String(value).padStart(2,"0")
+}
 
-function resetclock() {}
+function settime() {
+    const minutes=Math.floor(secondselapsed/60)
+    const seconds=secondselapsed%60
+    time.innerHTML=`${padding(minutes)}:${padding(seconds)}`;
+}
+
+function timer() {
+    secondselapsed=secondselapsed+1
+    settime();
+}
+
+function startclock() {
+    if (interval) stopclock();
+    interval=setInterval(timer,1000);
+}
+
+function stopclock() {
+    clearInterval(interval)
+}
+
+function resetclock() {
+    stopclock()
+    secondselapsed=0
+    settime()
+}
