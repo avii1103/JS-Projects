a0136c55ce40fe3907cd6f8a0344c63709bebf8e
Avichal <Avichal1108@gmail.com>
2025-09-07 20:39:57 +0530

    Update index.js

--- Stopwatch/index.js
+++ Stopwatch/index.js
@@ -1,25 +1,25 @@
-let secondselapsed=0;
-let interval=null;
-const time=document.getElementById("time");
+let secondsElapsed = 0;
+let interval = null;
+const time = document.getElementById("time")
 
-function padding(value); {
-    return String(value).padStart(2,"0")
+function padStart(value) {
+    return String(value).padStart(2, "0")
 }
 
-function settime() {
-    const minutes=Math.floor(secondselapsed/60)
-    const seconds=secondselapsed%60
-    time.innerHTML=`${padding(minutes)}:${padding(seconds)}`;
+function setTime() {
+    const minutes = Math.floor(secondsElapsed / 60)
+    const seconds = secondsElapsed % 60
+    time.innerHTML = `${padStart(minutes)}:${padStart(seconds)}` ;
 }
 
 function timer() {
-    secondselapsed=secondselapsed+1
-    settime();
+    secondsElapsed++;
+    setTime()
 }
 
 function startClock() {
-    if (interval) stopClock();
-    interval=setInterval(timer,1000);
+    if (interval) stopClock()
+    interval = setInterval(timer, 1000)
 }
 
 function stopClock() {
@@ -28,6 +28,6 @@ function stopClock() {
 
 function resetClock() {
     stopClock()
-    secondselapsed=0
-    settime()
+    secondsElapsed = 0;
+    setTime()
 }
